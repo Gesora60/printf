03ee3fe883f16978e2f498bd5c562f280e857f1d
Gesora60 <mosmay60@gmail.com>
2023-10-16 03:55:47 -0700

    fixed betty

--- _printf.c
+++ _printf.c
@@ -8,47 +8,52 @@
 
 int _printf(const char *format, ...)
 {
-  va_list args;
-  va_start(args, format);
-  int count = 0;
-  
-  while (*format != '\0')
-    {
-      if (*format == '%')
-	{
-	  _putchar(*format);
-	  count++;
-	}
-      else
-	{
-	  format++;
-	  if (*format == '0') break;
-	  if (*format == 'c')
-	    {
-	      int c = va_arg(args, int);
-	      _putchar(c);
-	      count++;
-	    }
-	  else if (*format == 's')
-	    {
-	      char *s = va_arg(args, char *);
-	      while (*s)
-		{
-		  _putchar(*s);
-		  s++;
-		  count++;
-		}
-	    }
-	  else (*format == '%')
-		 {
-		   _putchar('%');
-		   count++;
-		 }
-	}
-      format++;
-    }
-  va_end(args);
-  return count;
-}
+	va_list args;
 
+	va_start(args, format);
+
+	int count = 0;
+
+	while (*format != '\0')
+	{
+		if (*format == '%')
+		{
+			_putchar(*format);
+			count++;
+		}
+		else
+		{
+			format++;
+			if (*format == '0')
+				break;
+			if (*format == 'c')
+			{
+				int c = va_arg(args, int);
+
+				_putchar(c);
+
+				count++;
+			}
+			else if (*format == 's')
+			{
+				char *s = va_arg(args, char *);
+
+				while (*s)
+				{
+					_putchar(*s);
+					s++;
+					count++;
+				}
+			}
+			else if (*format == '%')
+			{
+				_putchar('%');
+				count++;
+			}
+		}
+		format++;
+	}
+	va_end(args);
+	return (count);
+}
 
