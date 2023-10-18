ca8ae5fead6fb2334c72f078cb0768c4591edfde
Gesora60 <mosmay60@gmail.com>
2023-10-18 02:58:10 -0700

    correction

--- _printf.c
+++ _printf.c
@@ -1,36 +1,47 @@
 #include "main.h"
-#include <stdarg.h>
 #include <unistd.h>
-
 /**
- * _printf - implementation of the inbuilt printf
- * @format: the format specifier
- * Return: the formated string
+ * _printf - Emulate the original.
+ *
+ * @format: Format by specifier.
+ *
+ * Return: count of chars.
  */
-
 int _printf(const char *format, ...)
 {
-	int printed = 0;
-
+	int i = 0, count = 0, count_fun;
 	va_list args;
 
 	va_start(args, format);
-
-	while (*format != '\0')
+	if (!format || (format[0] == '%' && !format[1]))
+		return (-1);
+	if (format[0] == '%' && format[1] == ' ' && !format[2])
+		return (-1);
+	while (format[i])
 	{
-		if (*format == '%')
+		count_fun = 0;
+		if (format[i] == '%')
 		{
-			format++;
-			printed = selector(format, args, printed);
-			format++;
+			if (!format[i + 1] || (format[i + 1] == ' ' && !format[i + 2]))
+			{
+				count = -1;
+				break;
+			}
+			count_fun += get_function(format[i + 1], args);
+			if (count_fun == 0)
+				count += _putchar(format[i + 1]);
+			if (count_fun == -1)
+				count = -1;
+			i++;
 		}
 		else
 		{
-			_putchar(*format);
-			printed++;
-			format++;
+			(count == -1) ? (_putchar(format[i])) : (count += _putchar(format[i]));
 		}
+		i++;
+		if (count != -1)
+			count += count_fun;
 	}
 	va_end(args);
-	return (printed);
+	return (count);
 }
